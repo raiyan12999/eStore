784c9f0516f2d5bc17bb762af047b6dbfc7a323b
raiyan12999 <raiyanbinatik.m29@gmail.com>
2025-07-21 10:05:06 -0700

    created product model

--- src/main/java/com/webApp/eStore/model/Product.java
+++ src/main/java/com/webApp/eStore/model/Product.java
@@ -5,4 +5,37 @@ public class Product {
     private int prodId;
     private String name;
     private int value;
+
+    public Product(int prodId, String name, int value){
+        this.prodId = prodId;
+        this.name = name;
+        this.value = value;
+    }
+
+
+
+
+    public int getProdId() {
+        return prodId;
+    }
+
+    public void setProdId(int prodId) {
+        this.prodId = prodId;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
 }
