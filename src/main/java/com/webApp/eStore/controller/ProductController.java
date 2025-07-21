7f72fbf3cfd0f5e9f83bfa5bd56d4565c350e88e
raiyan12999 <raiyanbinatik.m29@gmail.com>
2025-07-21 10:52:58 -0700

    Added functionality to add and show new product in a the list

--- src/main/java/com/webApp/eStore/controller/ProductController.java
+++ src/main/java/com/webApp/eStore/controller/ProductController.java
@@ -3,8 +3,7 @@ package com.webApp.eStore.controller;
 import com.webApp.eStore.model.Product;
 import com.webApp.eStore.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,8 +13,13 @@ public class ProductController {
     @Autowired
     ProductService service;
 
-    @RequestMapping("/products")
+    @GetMapping ("/products")
     public List<Product> getAllProducts(){
         return service.getProducts();
+    }
+
+    @PostMapping("/products")
+    public void addAProduct(@RequestBody Product prod){
+        service.addProduct(prod);
     }
 }
--- src/main/java/com/webApp/eStore/service/ProductService.java
+++ src/main/java/com/webApp/eStore/service/ProductService.java
@@ -3,6 +3,7 @@ package com.webApp.eStore.service;
 import com.webApp.eStore.model.Product;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -13,7 +14,7 @@ public class ProductService {
     Product product2 = new Product(102, "Desktop", 5000);
     Product product3 = new Product(103, "Phone", 4000);
 
-    List<Product> products = Arrays.asList(product1, product2, product3);
+    List<Product> products = new ArrayList<>(Arrays.asList(product1, product2, product3));
 
 //    List<Product> products = Arrays.asList(new Product(101, "Laptop", 3000),
 //                                            new Product(102, "Desktop", 5000),
@@ -21,5 +22,9 @@ public class ProductService {
 
     public List<Product> getProducts(){
         return products;
+    }
+
+    public void addProduct (Product prod){
+        products.add(prod);
     }
 }
