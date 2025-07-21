ce5f598cac9ee250f6736f9d70ccc764779753ed
raiyan12999 <raiyanbinatik.m29@gmail.com>
2025-07-21 10:16:50 -0700

    created product service

--- src/main/java/com/webApp/eStore/service/ProductService.java
+++ src/main/java/com/webApp/eStore/service/ProductService.java
@@ -1,5 +1,23 @@
 package com.webApp.eStore.service;
 
+import com.webApp.eStore.model.Product;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class ProductService {
 
+//    Product product1 = new Product(101, "Laptop", 3000);
+//    Product product2 = new Product(102, "Desktop", 5000);
+//    Product product3 = new Product(103, "Phone", 4000);
+//
+//    List<Product> products = Arrays.asList(product1, product2, product3);
+
+    List<Product> products = Arrays.asList(new Product(101, "Laptop", 3000),
+                                            new Product(102, "Desktop", 5000),
+                                            new Product(103, "Phone", 2000));
+
+    public List<Product> getProducts(){
+        return products;
+    }
 }
