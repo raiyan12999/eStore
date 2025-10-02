5208c103c58e2a4040f6bac05c44914e1b924d6a
raiyan12999 <raiyanbinatik.m29@gmail.com>
2025-10-02 11:42:19 -0700

    Added functionality for getting all the tags

--- src/main/java/com/webApp/eStore/controller/TagController.java
+++ src/main/java/com/webApp/eStore/controller/TagController.java
@@ -1,10 +1,14 @@
 package com.webApp.eStore.controller;
 
+import com.webApp.eStore.model.Tag;
 import com.webApp.eStore.service.TagService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -13,4 +17,9 @@ public class TagController {
 
     @Autowired
     public TagService service;
+
+    @GetMapping("/tags")
+    public List<Tag> getAllTags(){
+        return service.getTags();
+    }
 }
--- src/main/java/com/webApp/eStore/service/TagService.java
+++ src/main/java/com/webApp/eStore/service/TagService.java
@@ -1,12 +1,19 @@
 package com.webApp.eStore.service;
 
+import com.webApp.eStore.model.Tag;
 import com.webApp.eStore.repository.TagRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TagService {
 
     @Autowired
     public TagRepo repo;
+
+    public List<Tag> getTags() {
+        return repo.findAll();
+    }
 }
